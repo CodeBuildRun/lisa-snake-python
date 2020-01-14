28bffc84a8b4447d9b6509e20cb5a4f654c832bb
Jesús Hernández Tapia <jesusht@gmail.com>
2020-01-13 23:14:33 -0800

    Breaks down the Snake.Move function into multiple smaller sequential functions so that it is easier to implement AI modularly

--- app/snake.py
+++ app/snake.py
@@ -16,69 +16,110 @@ class Snake:
 
     def __init__(self):
         """
-        Snake Constructor.
+        Snake Constructor
         Initializes stateful snake
         """
 
-    def color(self):
+    def initialize(self, data):
+        """
+        Initializes stateful snake
+        """
+        # Get board information
+        game_board = data["board"]
+        # Determine board dimensions
+        self.board_width = game_board["width"]
+        self.board_height = game_board["height"]
+
         return Snake.snake_color
 
     def move(self, data):
-
-        print(json.dumps(data))
+        """
+        Move the snake to a chosen square
+        """
+        # print(json.dumps(data))
 
         # Get the board's current state
         game_board = data["board"]
-        # get coordinates of my snake head
-        my_head = data["you"]["body"][0]
-        my_head_x = my_head["x"]
-        my_head_y = my_head["y"]
 
-        # Make a list of all the bad coordinates and try to avoid them
-        height = game_board["height"]
-        width = game_board["width"]
+        # Make a list of all the board locations to avoid
+        bad_coords = self.get_bad_coords(game_board)
+
+        # get coordinates of my snake head
+        my_head = self.get_snake_head(data)
+
+        # Determine direction of movement
+        direction = self.chose_direction(my_head, bad_coords)
+        return direction
+
+    def get_snake_head(self, data):
+        """
+        Get coordinates of the snake's head
+        """
+        my_head_json = data["you"]["body"][0]
+        my_head = (my_head_json["x"], my_head_json["y"])
+        return my_head
+
+    def get_bad_coords(self, game_board):
+        """
+        Make a list of all the coordinates to avoid
+        """
         bad_coords = []
 
         # Bad coordinates that my snake needs to avoid
-        # 1. above and below the board
-        for x in range(width):
-            bad = (x, -1)
-            bad_coords.append(bad)
-            bad = (x, height)
-            bad_coords.append(bad)
-
-        # 2. left and right to the board
-        for y in range(height):
-            bad = (-1, y)
-            bad_coords.append(bad)
-            bad = (width, y)
-            bad_coords.append(bad)
-
-        # 3. snake bodies on the board
+        # 1. snake bodies on the board
         for snake in game_board["snakes"]:
             for body in snake["body"]:
                 bad = (body["x"], body["y"])
                 bad_coords.append(bad)
+        return bad_coords
 
+    def is_coord_on_board(self, coord):
+        """
+        Determines if a given coordinate is on the board
+        RETURNS: True if coordinate is on the board, False otherwise
+        """
+        on_board = False
+
+        if (coord[0] >= 0 and coord[0] < self.board_width) and \
+           (coord[1] >= 0 and coord[1] < self.board_height):
+            on_board = True
+
+        return on_board
+
+    def chose_direction(self, my_head, bad_coords):
+        """
+        Choses a direction to move based on the position of the snake's head
+        and a list of squares to avoid
+        Inputs:
+            my_head   : A tuple including the coordinates of the snake's head
+            bad_coords: A list of squares in the board that should be avoided
+        Returns:
+            direction : The chosen direction to move [UP, DOWN, LEFT, RIGHT]
+        """
+        # Create a list that will contain all possible moves
         possible_moves = []
 
         # Determine direction of movement
         # up
-        coord = (my_head_x, my_head_y - 1)
-        if coord not in bad_coords:
-            possible_moves.append(Moves.UP)
+        coord = (my_head[0], my_head[1] - 1)
+        if self.is_coord_on_board(coord):
+            if coord not in bad_coords:
+                possible_moves.append(Moves.UP)
         # down
-        coord = (my_head_x, my_head_y + 1)
-        if coord not in bad_coords:
-            possible_moves.append(Moves.DOWN)
+        coord = (my_head[0], my_head[1] + 1)
+        if self.is_coord_on_board(coord):
+            if coord not in bad_coords:
+                possible_moves.append(Moves.DOWN)
         # left
-        coord = (my_head_x - 1, my_head_y)
-        if coord not in bad_coords:
-            possible_moves.append(Moves.LEFT)
+        coord = (my_head[0] - 1, my_head[1])
+        if self.is_coord_on_board(coord):
+            if coord not in bad_coords:
+                possible_moves.append(Moves.LEFT)
         # right
-        coord = (my_head_x + 1, my_head_y)
-        if coord not in bad_coords:
-            possible_moves.append(Moves.RIGHT)
+        coord = (my_head[0] + 1, my_head[1])
+        if self.is_coord_on_board(coord):
+            if coord not in bad_coords:
+                possible_moves.append(Moves.RIGHT)
 
         # possible moves
         if len(possible_moves) > 0:
