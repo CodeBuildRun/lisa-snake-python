a0b7346ca944df5b56747c4073cfa2036a808cac
Vivian Wang <SESA186677@schneider-electric.com>
2020-01-19 19:35:03 -0800

    started adding algorithm of chasing tail with consideration of health

--- app/snake.py
+++ app/snake.py
@@ -53,6 +53,7 @@ class Snake:
 
         # find a path to the target
         path = self.finder.find_path(start_loc, end_loc, grid)
+        print(path)
 
         # Determine direction of movement
         direction = self.chose_direction(my_head, path)
@@ -65,6 +66,21 @@ class Snake:
         my_head_json = data["you"]["body"][0]
         my_head = (my_head_json["x"], my_head_json["y"])
         return my_head
+
+    def get_snake_tail(self, data):
+        """
+        Get coordinates of the snake's tail
+        """
+        my_tail_json = data["you"]["body"][-1]
+        my_tail= (my_tail_json["x"], my_tail_json["y"])
+        return my_tail
+
+    def get_snake_health(self, data):
+        """
+        Get coordinates of the snake's tail
+        """
+        my_health = data["you"]["health"]
+        return my_health
 
     def set_bad_coords(self, game_board, matrix):
         """
@@ -113,6 +129,7 @@ class Snake:
         # Check that the path has more than one step so we know we have not yet
         # arrived to the destination. A path with only one step indicates that
         # we are already located on the target destination
+
         if len(path[0]) >= 2:
             # the first element in the list is always the current location, so
             # we take the second element, which represents the first move
@@ -126,8 +143,10 @@ class Snake:
                 direction = Moves.DOWN
             elif vector == (1, 0):
                 direction = Moves.RIGHT
-            else:
+            elif vector == (-1,0):
                 direction = Moves.LEFT
+            else:
+                direction = random.choice(list(Moves))
         else:
             # TODO: Check the case when the target location is occupied
             # by a snake. In this case, the path will be empty
@@ -143,10 +162,16 @@ class Snake:
         RETURNS: a tuple containing the coordinates of the target
         """
         # chose an apple
-        food = data["board"]["food"]
-        if len(food) > 0:
+        if data["turn"]<10 or self.get_snake_health(data)<50:
+       
+            food = data["board"]["food"]
             apple = food[0]
             target = (apple["x"], apple["y"])
+
+
         else:
-            target = random.choice([(14, 14), (0, 0), (0, 14), (14, 0)])
+            
+            target = self.get_snake_tail(data)
+            
+ 
         return target
